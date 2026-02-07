b740228ebab7e86c3c02ef87594de660e49f52dd
Remon Ayman <R.Allah18419@student.aast.edu>
2026-02-07 10:44:17 +0200

    chaneeeeeeeew

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase warning limit to 1000kb
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'] // Split React into its own chunk
+        }
+      }
+    }
+  }
 })
